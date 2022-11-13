ad87935d2106b7ab80d067b337ed406f0c0a6c1e
TurTung <755126039@qq.com>
2022-11-13 09:20:15 +0800

    2022.11.13

--- javascript/innerHTML.js
+++ javascript/innerHTML.js
@@ -10,7 +10,18 @@ const bannerWH = function () {
   bannerDiv.style.height = ((bannerW / 6000) * 3375) + 'px';
   return
 }
+const Img = function () {
+  let logo = document.querySelector('.Logo-Img').src = 'images/logo.jpg';
+  let Nav = document.querySelectorAll('.Navigation > ul > li > a > div > img');
+  let NavImg = ['主页','主页','主页']
+  let NavLength = Nav.length;
+  for (let i = 0 ; i < NavLength; i++) {
+    Nav[i].src = 'images/' + NavImg[i] + '.png';
+  }
+  let banner = document.querySelector('.banner').style = 'background-image  :url("images/banner.jpg");';
+}
 window.onload = function() {
+  Img();
   bannerWH();
   return;
 };
