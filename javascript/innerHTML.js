3d89d9f4ea4c47ae6be213753a17d25dd7842cc3
TurTung <755126039@qq.com>
2022-11-13 08:54:14 +0800

    2022.11.13-banner整理

--- javascript/innerHTML.js
+++ javascript/innerHTML.js
@@ -1,7 +1,20 @@
 "use strict";
+let banner;
+const bannerTimeout = function() {
+  clearTimeout(banner);
+  banner = setTimeout(bannerWH , 50);
+}
+const bannerWH = function () {
+  let bannerDiv = document.querySelector('.banner');
+  let bannerW = bannerDiv.clientWidth;
+  bannerDiv.style.height = ((bannerW / 6000) * 3375) + 'px';
+  return
+}
 window.onload = function() {
+  bannerWH();
   return;
 };
 window.onresize = function() {
+  bannerTimeout();
   return;
 }
