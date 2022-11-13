96ee2ca89094b280dc8a256add006670d083cf65
TurTung <755126039@qq.com>
2022-11-13 14:51:22 +0800

    2022.11.13-内容扩展

--- javascript/innerHTML.js
+++ javascript/innerHTML.js
@@ -30,16 +30,33 @@ const Img = function () {
   return
 }
 const quotesSpan = function () {
-  let arr = ['静,不是耳旁无声,而是心里无争.'];
+  let arr = ['静,不是耳旁无声,而是心里无争.',
+'或许今年的冬天更值得期待.',
+'你有时间去试错,也有时间去止损,不应该拿时间去后悔.',
+'岁寒,时深,添衣,勿病.',
+'白日莫空过,青春不再来.',
+'谁虚度了年华,青春就将褪色.',
+'未来不足惧,过往不须泣.',
+'终日乾乾,与时偕行.',
+'明者因时而变，知者随事而制.',
+'君子务本,本立而道生.',
+'兼听则明,偏信则暗',
+'尽信书,则不如无书.',
+'款款独行,才不致倾溢.',
+'凡事之本,必先治身.',
+'不傲才以骄人,不以宠而作威.',
+'万里星光,一如既往.',
+'思考时,要像一位智者;但讲话时,要像一位普通人.'
+];
   let arrLength = arr.length;
   let num = Math.floor(Math.random() * arrLength );
   quotes.innerHTML = '<span>' + arr[num] + '</span>';
   return quotesSpan;
 }
 window.onload = function() {
-  setInterval(quotesSpan(),180000);
   Img();
   bannerWH();
+  setInterval(quotesSpan(),180000);
   return;
 };
 window.onresize = function() {
