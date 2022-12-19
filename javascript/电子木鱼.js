6f2165711dbaa632aedaed4619f3573b7d9a510a
TurTung <755126039@qq.com>
2022-12-19 21:37:03 +0800

    2022.12.19-L2高度定义

--- javascript/电子木鱼.js
+++ javascript/电子木鱼.js
@@ -12,11 +12,11 @@ let ElectronicWoodFish = JSON.parse(localStorage.getItem('ElectronicWoodFish')) 
 const item = document.querySelector('.num-item');
 const WoodFishClick = function (num) {
   ElectronicWoodFish.value = ElectronicWoodFish.value + num ;
-  item.textContent = ElectronicWoodFish.value;
+  item.textContent = ElectronicWoodFish.value.toLocaleString();
 }
 document.querySelector('.wood-fish > img').onclick = () => {return WoodFishClick(ElectronicWoodFish.num)};
 const load = () => {
-  item.textContent = ElectronicWoodFish.value;
+  item.textContent = ElectronicWoodFish.value.toLocaleString();
   let setting = [document.querySelector('#setting-title') , document.querySelector('#setting-num')]
   setting[0].value = ElectronicWoodFish.title;
   setting[1].value = ElectronicWoodFish.num;
